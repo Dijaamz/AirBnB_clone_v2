d7bf8c7b68163bac9e3a6175460181a4539ba5fa
Dijaamz <khadija17amzoug@gmail.com>
2023-10-26 08:19:57 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,47 @@
 #!/usr/bin/python3
-"""
-Fabric script to deploy tgz archive
-fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
-    -i private-key -u user
-"""
+# fabric script that deploy a file it to servers
+""" Fabric script to deploy """
+import os
+from fabric.api import *
+from datetime import datetime
 
-from os.path import exists
-from fabric.api import put, run, env
-
-env.hosts = ['35.243.128.200', '3.239.120.96']
+env.hosts = ['35.185.87.254', '52.72.127.245']
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/holberton"
+env.warn_only = True
 
 
 def do_deploy(archive_path):
-    """
-    copies archive file from local to my webservers
-    """
-
-    if not exists(archive_path):
+    """ upload to web servers and deploy """
+    if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
         return False
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        put(archive_path, "/tmp/")
+        # get file name without extension
+        filename = os.path.splitext(os.path.basename(archive_path))[0]
+        # upload to /tmp dir to server
+        put(local_path=archive_path, remote_path="/tmp")
+        # create destination directory
+        run("mkdir -p /data/web_static/releases/" + filename + "/")
+        # uncompress tar file to a directory
+        run("sudo tar -xzf /tmp/" + filename + ".tgz" +
+            " -C /data/web_static/releases/" + filename + "/")
+        # Delete file uploaded
+        run("rm /tmp/" + filename + ".tgz")
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        # move files to a previous folder
+        run("mv /data/web_static/releases/" + filename +
+            "/web_static/* /data/web_static/releases/" + filename + "/")
 
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(file_name, file_name))
+        # delete that folder
+        run("rm -rf /data/web_static/releases/" + filename +
+            "/web_static")
 
-        run('rm -rf /tmp/{}.tgz'.format(file_name))
-
-        run(('mv /data/web_static/releases/{}/web_static/* ' +
-            '/data/web_static/releases/{}/')
-            .format(file_name, file_name))
-
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(file_name))
-
-        run('rm -rf /data/web_static/current')
-
-        run(('ln -s /data/web_static/releases/{}/' +
-            ' /data/web_static/current')
-            .format(file_name))
+        # delete symbolic link /data/web_static/current
+        run("rm -rf /data/web_static/current")
+        # create a new symbolic link
+        run("ln -s /data/web_static/releases/" + filename +
+            "/ /data/web_static/current")
+        print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
